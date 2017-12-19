2eaeae5f3ceaa56f0a5febab532190e677a2ba52
ingridsag <starlling@hotmail.com>
2017-12-19 01:58:43 -0600

    cambios

--- js/app.js
+++ js/app.js
@@ -1,6 +1,48 @@
 /*
  * Funcionalidad de tu producto
  */
+ /listar las ciudades
+ for (ciudad in data) {
+     console.log(ciudad);
+ }
 
+ //listar las generaciones
+ for (ciudad in data) {
+     console.log( ciudad , '.- ', data[ciudad] );
+ }
+
+
+
+
+ //listar las estudiant@s de la generacion
+ for (ciudad in data) {
+     console.log( ciudad , '.- ', data[ciudad] );
+     var periodo = data[ciudad];
+     for (per in periodo) {
+         console.log('periodo: ',per );
+         console.log('alumnas: ',  data[ciudad][per] );
+     }
+ }
+
+
+ //listar las estudiant@s de la generacion
+ for (ciudad in data) {
+     console.log( ciudad , '.- ', data[ciudad] );
+     var periodo = data[ciudad];
+     for (per in periodo) {
+         console.log('periodo: ',per );
+         console.log('alumnas (datos personales): ',  data[ciudad][per] );
+         var students = data[ciudad][per];
+         for (student in students ) {
+             console.log('student: ', student);
+             for (s in student) {
+                 console.log(' datos estudiante (Sptrint): ' , data[ciudad][per].students);
+             }
+         }
+     }
+ }
+
+
+ console.log(' datos  de estudiante de CDMX , primer periodo : ' , data['CDMX']['2017-1'].students[0]);
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
