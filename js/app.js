de95cee697ffc49bec969f1b67a7c043940d5559
ingridsag <starlling@hotmail.com>
2017-12-20 04:46:25 -0600

    nuevos intentos

--- js/app.js
+++ js/app.js
@@ -1,27 +1,49 @@
-/*
- * Funcionalidad de tu producto
+ /*
+ * Funcionalidad del producto
  */
- /listar las ciudades
+ /*listar las ciudades*/
  for (ciudad in data) {
      console.log(ciudad);
  }
 
+ for (ciudad in data) {
+     console.log( ciudad , '.- ', data[ciudad] );
+ }
+
+
+ //listar las estudiant@s de la generacion
+ var obtenerData= function( students, sede, generacion){
+   document.getElementByClass("container1").innerHTML="";
+   var show = datps[sede]["data-generation"][generacion]["students"];
+
+ for (var i=0 ; i<data-sede.length; i ++) {
+     var periodo = data[sede];
+      show(sede.length, generation);
+     }
+ }
+/*
  //listar las generaciones
  for (ciudad in data) {
      console.log( ciudad , '.- ', data[ciudad] );
  }
 
 
-
-
  //listar las estudiant@s de la generacion
- for (ciudad in data) {
+ /*for (ciudad in data) {
      console.log( ciudad , '.- ', data[ciudad] );
      var periodo = data[ciudad];
      for (per in periodo) {
          console.log('periodo: ',per );
-         console.log('alumnas: ',  data[ciudad][per] );
+        var test= console.log('alumnas: ',  data[ciudad][per] );
      }
+     */
+     /*
+     for []
+     var matricula= document.getElementById("enrollement");
+     var ptext=document.createElement("p");
+     var text= document.createTextNode(test);
+     ptext.appendChild(text);
+     matricula.appendChild(ptext);
  }
 
 
@@ -46,3 +68,4 @@
  console.log(' datos  de estudiante de CDMX , primer periodo : ' , data['CDMX']['2017-1'].students[0]);
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
+*/
